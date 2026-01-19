2ff9bf1c06fb30e9160e52667923c130be7d8eb6
EmersonRodas9029 <theripper446@gmail.com>
2026-01-18 18:34:03 -0600

    Configuración inicial del proyecto

---
+++ src/main/java/com/codepuppeteer/sistema_tienda_desktop/SistemaTiendaDesktopApplication.java
@@ -0,0 +1,13 @@
+package com.codepuppeteer.sistema_tienda_desktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTiendaDesktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTiendaDesktopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codepuppeteer/sistema_tienda_desktop/SistemaTiendaDesktopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepuppeteer.sistema_tienda_desktop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTiendaDesktopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
